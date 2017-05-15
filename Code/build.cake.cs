5695636a6e2f9d5792abe62cef443d9bea8fd850
Jonathon Smith <failing@crashdive.co.uk>
2017-05-15 18:18:28 +0100

    Changed ex for Code Highlighting + Added Cake Ex

---
+++ Code/build.cake.cs
@@ -0,0 +1,23 @@
+﻿#addin nuget:?package=Cake.Scripty
+#tool nuget:?package=Scripty
+
+var target = Argument("target", "default");
+
+////
+//Scripty
+////
+
+Task("scripty-run-task")
+.Does(() => {
+    Scripty(@"PathToProject.csproj")
+        .Evaluate(@"PathToScriptyFile.csx");
+});
+
+////
+//Default
+////
+
+Task("default")
+.IsDependentOn("scripty-run-task")
+
+RunTarget(target);
